eebfbbd72646a6abcf7ae9632d0d21dd598790c5
kSabchev <kaloyansabchev@gmail.com>
2020-08-27 23:34:00 +0300

    Home, Header

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import './Home.js';
+import Home from './Home.js';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="app">
       <h1>lets build airbnb-clone</h1>
-
-    {/* Home */}
-      { /* Header */}
-        {/* Search*/}
+      <Header/>
+        {/* Search */}
+      <Home/>
+     
+    
+    
       { /* Banner */}
       { /* Cards */}
       { /* Footer */}
---
+++ airbnb-clone/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <img
+            className="header__icon"
+            src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+            />
+        </div>
+    )
+}
+export default Header
