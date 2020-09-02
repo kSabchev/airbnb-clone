b807247dee570591ee4e069719bb49a08b95f86b
kSabchev <kaloyansabchev@gmail.com>
2020-09-02 23:08:38 +0300

    added links

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,30 +1,28 @@
 import React, { useState } from 'react'
-import './Banner.css';
+import './Banner.css'
 import { Button } from "@material-ui/core";
 import Search from './Search';
+import { useHistory } from "react-router-dom";
 
 function Banner() {
+    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
         <div className='banner'>
             <div className='banner__search'>
-                {showSearch && <h1> <Search /> </h1>}
-                <Button onClick={()=>setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+                {showSearch && <Search />}
+
+                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
                     {showSearch ? "Hide" : "Search Dates"}
-
                 </Button>
-
             </div>
-            <div className="banner__info">
+            <div className='banner__info'>
                 <h1>Get out and stretch your imagination</h1>
                 <h5>
-                    Plan a different kind of getway to uncover the gems near you.
+                    Plan a different kind of getaway to uncover the hidden gems near you.
                 </h5>
-                <Button
-                variant='outlined'>
-                    Explore Nearby
-                </Button>
+                <Button onClick={() => history.push('/search')} variant='outlined'>Explore Nearby</Button>
             </div>
         </div>
     )
