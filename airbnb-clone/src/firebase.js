2d202c738526c8c8950c902bbb211ddaa4ac0d2c
kSabchev <kaloyansabchev@gmail.com>
2020-09-10 19:28:42 +0300

    firebase

---
+++ airbnb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWLAw8eErPFwRAutKCIn8kBKUi6zVn4Oc",
+    authDomain: "react-airbnb-clone-3c87d.firebaseapp.com",
+    databaseURL: "https://react-airbnb-clone-3c87d.firebaseio.com",
+    projectId: "react-airbnb-clone-3c87d",
+    storageBucket: "react-airbnb-clone-3c87d.appspot.com",
+    messagingSenderId: "886352041396",
+    appId: "1:886352041396:web:49a0929acd59c4ab184ee1",
+    measurementId: "G-XENQ4BKBXD"
+  };
